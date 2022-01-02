a733f60b4fb3c031165074b04319921b5e7a1b3d
Peter Thao <peterthao@Peters-iMac.local>
2022-01-02 11:03:57 -0500

    add friend and reaction to resolvers mutation

--- server/server.js
+++ server/server.js
@@ -5,6 +5,9 @@ const { ApolloServer } = require('apollo-server-express');
 //import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 
+//import middleware function
+const { authMiddleware } = require('./utils/auth');
+
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -18,7 +21,8 @@ const startServer = async () => {
   //create a new Apollo server and pass in our schema data
   const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: authMiddleware
   });
 
   //start the Apollo server
